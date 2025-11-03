3892b960f1c458b6300f8aca0e6f2ed820384c42
“im-minji” <mjlim514@gmail.com>
2025-11-03 16:29:18 +0900

    feat: 뷰 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,20 +1,21 @@
-package lotto;
+package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class inputView {
-    // 입출력 요구사항
+public class InputView {
 
-    //- 사용자에게 로또 구입 금액 입력 받기 (단위: 1,000원)
+    public String readPurchaseAmount() {
+        System.out.println("구입금액을 입력해 주세요.");
+        return Console.readLine();
+    }
 
-    //- 구입 금액에 해당하는 만큼 발행한 로또 수량 및 번호를 출력하기
+    public String readWinningNumbers() {
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
 
-
-    //- 로또 번호는 오름차순으로 정렬하여 출력하기
-    //
-    //
-    //- 사용자에게 당첨 번호(6개)를 입력 받기 (쉼표를 기준으로 구분하기)
-    //
-    //
-    //- 사용자에게 보너스 번호(1개)를 입력 받기
+    public String readBonusNumber() {
+        System.out.println("\n보너스 번호를 입력해 주세요.");
+        return Console.readLine();
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,52 @@
 package lotto.view;
 
+import java.util.List;
+import java.util.Map;
+import lotto.model.Lotto;
+import lotto.model.LottoRank;
+
 public class OutputView {
+
+    public void printErrorMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printPurchasedLottos(int count, List<Lotto> lottos) {
+        System.out.println("\n" + count + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public void printStatisticsHeader() {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+    }
+
+    public void printStatistics(Map<LottoRank, Integer> statistics) {
+        List<LottoRank> ranksToPrint = List.of(
+                LottoRank.FIFTH, LottoRank.FOURTH, LottoRank.THIRD,
+                LottoRank.SECOND, LottoRank.FIRST
+        );
+
+        for (LottoRank rank : ranksToPrint) {
+            String message = formatRankMessage(rank, statistics.get(rank));
+            System.out.println(message);
+        }
+    }
+
+    private String formatRankMessage(LottoRank rank, int count) {
+        String prize = String.format("%,d", rank.getWinningPrize());
+        String matchCount = rank.getWinningCount() + "개 일치";
+
+        if (rank.isNeedBonus()) {
+            matchCount += ", 보너스 볼 일치";
+        }
+
+        return String.format("%s (%s원) - %d개", matchCount, prize, count);
+    }
+
+    public void printRateOfReturn(double rate) {
+        System.out.printf("총 수익률은 %.1f%%입니다.%n", rate);
+    }
 }
